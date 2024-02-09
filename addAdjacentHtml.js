3a0d7160e2ffe899a9d5e92cc2ec27f4617f386c
unknown <jaydev.k@crestinfosystems.com>
2024-02-09 12:55:15 +0530

    final

--- addAdjacentHtml.js
+++
@@ -1,9 +0,0 @@
-let box = document.querySelector('.box');
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-arr.forEach((elem, i) => {
-  let html = `
-      <h2>${elem}</h2>
-    `;
-  box.insertAdjacentHTML('beforeend', html);
-});
--- timer.js
+++
@@ -1,9 +0,0 @@
-let time = 1500;
-
-let timer = setInterval(() => {
-  let sec = time % 60;
-  let min = Math.trunc(time / 60);
-  console.log(`${min}:${sec}`);
-  time--;
-  if (time == 1490) clearInterval(timer);
-}, 1000);
